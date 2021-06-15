3aab52867c9d714576bc4e34f58831312a89fba1
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-15 18:33:06 +0300

    Prioritized db over server running.

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,14 @@
 const app = require("../app");
+const db = require("../db/mongo-db");
+const { Port } = require("../helpers/constants");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || Port.default;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+}).catch((error) => {
+  console.log(`Error: ${error.message}.`);
 });
