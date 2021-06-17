978228829217a287d56eed4a005a8c3ed1001bb0
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-18 02:09:08 +0300

    Changed order of server starting and db connection.

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,9 @@
 const app = require("../app");
-const db = require("../db/mongo-db");
 const { Port } = require("../helpers/constants");
 require("dotenv").config();
 
 const PORT = process.env.PORT || Port.default;
 
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
-  });
-}).catch((error) => {
-  console.log(`Error: ${error.message}.`);
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
